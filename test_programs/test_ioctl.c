9b5399719586862504cd435b7e1203253c1cba25
Bogdan Bescherepov <bogdanrus@list.ru>
2024-06-15 05:59:12 +0300

    Update test_ioctl.c

--- test_programs/test_ioctl.c
+++ test_programs/test_ioctl.c
@@ -3,6 +3,7 @@
 #include <stdbool.h>
 #include <sys/ioctl.h>
 #include <time.h>
+#include <sys/types.h>
 
 #define     SCDRV_IOCTL_SET_IO_BLOCKING 				1
 #define     SCDRV_IOCTL_BUFFER_ACCESS_LAST_READ_TIME 	2
@@ -14,12 +15,33 @@
 
 int main(int argc, char* argv[])
 {
-	int fd = open("/dev/scdrv0", S_IRUSR);
-	struct timespec read_time;
+	int fd = open("/dev/scdrv0", S_IRUSR | S_IWUSR);
+	long int read_time;
+	long int write_time;
+	pid_t   last_write_pid;
+	uid_t   last_write_uid;
+	pid_t   last_read_pid;
+	uid_t   last_read_uid;
 
-	ioctl(fd, SCDRV_IOCTL_SET_IO_BLOCKING, false);
+	ioctl(fd, SCDRV_IOCTL_SET_IO_BLOCKING, 0);
 	printf("Check dmesg. SCDRV log about blocking mode must have been created.\n");
-	ioctl(fd, SCDRV_IOCTL_BUFFER_ACCESS_LAST_WRITE_TIME, &read_time);
-	printf("Last scdrv read time: %ld.%ld\n", read_time.tv_sec, read_time.tv_nsec);
+
+	ioctl(fd, SCDRV_IOCTL_BUFFER_ACCESS_LAST_READ_TIME, &read_time);
+	printf("Last scdrv read time: %ld\n", read_time);
+
+	ioctl(fd, SCDRV_IOCTL_BUFFER_ACCESS_LAST_WRITE_TIME, &write_time);
+	printf("Last scdrv write time: %ld\n", write_time);
+
+	ioctl(fd, SCDRV_IOCTL_BUFFER_ACCESS_LAST_READ_PID, &last_read_pid);
+	printf("Last scdrv read pid: %d\n", last_read_pid);
+
+	ioctl(fd, SCDRV_IOCTL_BUFFER_ACCESS_LAST_WRITE_PID, &last_write_pid);
+	printf("Last scdrv write pid: %d\n", last_write_pid);
+
+	ioctl(fd, SCDRV_IOCTL_BUFFER_ACCESS_LAST_READ_UID, &last_read_uid);
+	printf("Last scdrv read uid: %d\n", last_read_uid);
+
+	ioctl(fd, SCDRV_IOCTL_BUFFER_ACCESS_LAST_WRITE_UID, &last_write_uid);
+	printf("Last scdrv write uid: %d\n", last_write_uid);
 
 }
